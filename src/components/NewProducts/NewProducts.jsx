94502d5f0d2c74af8fa2dd659066e308af36b80a
Dewaley <adewaleademilua@gmail.com>
2022-10-16 19:03:13 +0100

    minor changes

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -3,9 +3,9 @@ import Button from "../Button/Button";
 
 const NewProducts = () => {
   return (
-    <div className='flex flex-col gap-12'>
+    <div className='flex flex-col items-center gap-12'>
       <h2 className='text-2xl text-center font-medium'>New Products</h2>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 md:w-[80vw]'>
         <div className='px-2 py-3 border-[1px] border-greyish flex flex-col gap-3 rounded'>
           <img
             src={advocado}
