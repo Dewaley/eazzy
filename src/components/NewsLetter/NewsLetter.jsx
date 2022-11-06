d2f409a230d985c476683931bf4383d6d1b478f6
Dewaley <adewaleademilua@gmail.com>
2022-11-06 12:51:02 +0100

    newsletter ish

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const NewsLetter = () => {
   const scriptURL =
-    "https://script.google.com/macros/s/AKfycbykrtMzDvGGzI6My0wtlXiqxLmuThXTgoUVC5NBSchza_1IixlVw1a-Z0AaP24OXqZuXA/exec";
+    "https://sheet.best/api/sheets/79e268df-2bbd-4bce-972c-62accf83f8ba";
 
   const {
     register,
@@ -13,18 +13,33 @@ const NewsLetter = () => {
     formState: { errors },
   } = useForm();
 
-  const [message,setMessage] = useState("")
+  const [message, setMessage] = useState("");
 
-  const onSubmit = (data) => {
-    fetch(scriptURL, { method: 'POST', body: data})
-      .then(response => {
-        console.log('Success!', response)
-        setMessage("Thanks for reaching out to us!")
-        setTimeout(() => {
-          setMessage("")
-        },3000)
-      })
-      .catch(error => console.error('Error!', error.message))
+  const onSubmit = async (data) => {
+    console.log(data)
+    return axios
+    .post(scriptURL, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      // return response;
+      console.log(response);
+    })
+    .catch((error) => {
+      // return error.response;
+      console.log(error);
+    });
+    // fetch(scriptURL, { method: "POST", body: data })
+    //   .then((response) => {
+    //     console.log("Success!", response);
+    //     setMessage("Thanks for reaching out to us!");
+    //     setTimeout(() => {
+    //       setMessage("");
+    //     }, 3000);
+    //   })
+    //   .catch((error) => console.error("Error!", error.message));
   };
 
   return (
@@ -46,7 +61,7 @@ const NewsLetter = () => {
           id=''
           placeholder='Your Name'
           className='border-[1px] border-greenish p-3 w-[80vw] rounded sm:w-[50vw] lg:w-[35vw] bg-transparent z-20'
-          {...register("name")}
+          {...register("Name")}
           required
         />
         <input
@@ -55,11 +70,11 @@ const NewsLetter = () => {
           id=''
           placeholder='Email Address'
           className='border-[1px] border-greenish p-3 rounded w-[80vw] sm:w-[50vw] lg:w-[35vw] bg-transparent z-20'
-          {...register("email")}
+          {...register("Email")}
           required
         />
         <Button content={"Subscribe"} type={"submit"} />
-        <p className="z-20">{message}</p>
+        <p className='z-20'>{message}</p>
       </form>
       <div className='!z-10 flex h-96 w-96 rounded-full border-[60px] border-[#7CE58D] absolute -left-[12rem] top-[50%] -translate-y-[50%]'></div>
       <div className='flex h-80 w-80 rounded-full border-[60px] border-[#7CE58D] absolute -right-[12rem] !z-10 -bottom-[12rem]'></div>
