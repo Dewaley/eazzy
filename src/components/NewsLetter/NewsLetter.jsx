843e4c72da65604c426baa594c4a8a0d733cae5f
Dewaley <adewaleademilua@gmail.com>
2022-11-06 14:21:02 +0100

    Waitlist done

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -1,51 +1,30 @@
 import Button from "../Button/Button";
-import { useForm } from "react-hook-form";
-import axios from "axios";
 import { useState } from "react";
 
 const NewsLetter = () => {
-  const scriptURL =
-    "https://sheet.best/api/sheets/79e268df-2bbd-4bce-972c-62accf83f8ba";
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const form = document.getElementById("my-form");
 
   const [message, setMessage] = useState("");
-
-  const onSubmit = async (data) => {
-    console.log(data)
-    return axios
-    .post(scriptURL, data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    .then((response) => {
-      // return response;
-      console.log(response);
-    })
-    .catch((error) => {
-      // return error.response;
-      console.log(error);
-    });
-    // fetch(scriptURL, { method: "POST", body: data })
-    //   .then((response) => {
-    //     console.log("Success!", response);
-    //     setMessage("Thanks for reaching out to us!");
-    //     setTimeout(() => {
-    //       setMessage("");
-    //     }, 3000);
-    //   })
-    //   .catch((error) => console.error("Error!", error.message));
-  };
 
   return (
     <div className='overflow-hidden relative w-full'>
       <form
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          const data = new FormData(form);
+          const action =
+            "https://script.google.com/macros/s/AKfycbw93XWQ4hlGTPsSo2FyrcU3Rn-9aS9O2TTFZXP5aFftIPEzjlsDnwYaTien6ef5O4uFgg/exec";
+          fetch(action, {
+            method: "POST",
+            body: data,
+          }).then(() => {
+            setMessage("Thanks for reaching out to us!");
+            setTimeout(() => {
+              setMessage("");
+            }, 3000);
+          });
+        }}
+        id='my-form'
         className='flex flex-col gap-6 items-center px-3 py-8 bg-[#EBF2EB] w-full overflow-hidden z-20'
       >
         <h1 className='font-medium text-center text-3xl z-20'>
@@ -61,7 +40,6 @@ const NewsLetter = () => {
           id=''
           placeholder='Your Name'
           className='border-[1px] border-greenish p-3 w-[80vw] rounded sm:w-[50vw] lg:w-[35vw] bg-transparent z-20'
-          {...register("Name")}
           required
         />
         <input
@@ -70,7 +48,6 @@ const NewsLetter = () => {
           id=''
           placeholder='Email Address'
           className='border-[1px] border-greenish p-3 rounded w-[80vw] sm:w-[50vw] lg:w-[35vw] bg-transparent z-20'
-          {...register("Email")}
           required
         />
         <Button content={"Subscribe"} type={"submit"} />
