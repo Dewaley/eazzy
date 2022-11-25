292626a55993ab2f615779017c4650645a258aee
Dewaley <adewaleademilua@gmail.com>
2022-11-25 04:58:20 +0100

    minor fix

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -1,7 +1,18 @@
 import advocado from "../../assets/advocado.png";
 import Button from "../Button/Button";
+import { useState } from "react";
 
 const NewProducts = () => {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    count > 1 && setCount((prev) => prev - 1);
+  };
+
   return (
     <div className='px-3 md:px-6 flex flex-col items-center gap-12 py-8'>
       <h2 className='text-3xl text-center font-medium'>New Products</h2>
@@ -12,13 +23,35 @@ const NewProducts = () => {
             alt=''
             className='w-full object-cover object-center'
           />
-          <div className='flex flex-col gap-1'>
+          <div className='flex flex-col gap-2'>
+            <span>Advocado</span>
             <p className='flex justify-between'>
-              <span>Advocado</span>
+              <span>#2500</span>
               <span>(each)</span>
             </p>
-            <span>#2500</span>
-            <Button content={"Add to Cart"} />
+            <span className='flex items-center justify-between gap-2'>
+              <button
+                className='text-lg text-white bg-greenish/60 flex justify-center items-center w-8 h-8 rounded'
+                onClick={() => decrement()}
+              >
+                -
+              </button>
+              <input
+                type='number'
+                name='quantity'
+                id='quantity'
+                className='border-[1px] rounded p-2 font-light w-[60%] h-8 text-center rounded'
+                value={count}
+                disabled
+              />
+              <button
+                className='text-lg text-white bg-greenish flex justify-center items-center w-8 h-8 rounded'
+                onClick={() => increment()}
+              >
+                +
+              </button>
+            </span>
+            <Button content={"Add to Cart"} large />
           </div>
         </div>
         <div className='px-2 py-3 border-[1px] border-greyish flex flex-col gap-3 rounded'>
