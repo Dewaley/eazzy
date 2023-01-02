547d021b7d3a31ce5bc7806beae91345f370762d
Dewaley <adewaleademilua@gmail.com>
2023-01-02 14:49:47 +0100

    initial order structure

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,23 +1,82 @@
 import PaymentServices from "../../services/PaymentServices";
 import { useState, useEffect } from "react";
+import AuthServices from "../../services/AuthServices";
+import PlaceholderLoading from "react-placeholder-loading";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [user, setUser] = useState({});
+  const [status, setStatus] = useState("All");
   useEffect(() => {
-    PaymentServices.fetchOrders(1)
-      .then((res) => {
-        const data = res?.data?.orders?.data?.map(async(item) => {
-          console.log(JSON.parse(item.order_details));
-          return PaymentServices.fetchSingleOrder(
-            item?.order_transaction_id
-          )
-        });
-          console.log(data)
-        setOrders(data)
-      })
+    AuthServices.fetchUser().then((res) => {
+      console.log(res?.data);
+      setUser(res?.data);
+    });
+    PaymentServices.fetchOrders(1).then(async (res) => {
+      console.log(res);
+      const data = await res?.data?.orders?.data;
+      console.log("aaa", JSON.parse(data));
+      setOrders(data);
+      return data;
+    });
   }, []);
 
-  return <div>Orders</div>;
+  return (
+    <div className='bg-[#EBF2EB] py-2 md:p-8'>
+      <div className='bg-white p-3 md:p-6 flex flex-col gap-6'>
+        <h1 className='font-semibold text-["rgba(52, 53, 56, 0.52)"]'>
+          {user?.first_name + "'s orders"}{" "}
+        </h1>
+        <div className='flex text-sm justify-between sm:justify-start sm:gap-3'>
+          <span
+            className={`hover:font-semibold cursor-pointer ${
+              status === "All" && "font-semibold"
+            }`}
+            onClick={() => {
+              setStatus("All");
+            }}
+          >
+            All
+          </span>
+          <span
+            className={`hover:font-semibold cursor-pointer ${
+              status === "Processing" && "font-semibold"
+            }`}
+            onClick={() => {
+              setStatus("Processing");
+            }}
+          >
+            Processing
+          </span>
+          <span
+            className={`hover:font-semibold cursor-pointer ${
+              status === "Shipped" && "font-semibold"
+            }`}
+            onClick={() => {
+              setStatus("Shipped");
+            }}
+          >
+            Shipped
+          </span>
+          <span
+            className={`hover:font-semibold cursor-pointer ${
+              status === "Completed" && "font-semibold"
+            }`}
+            onClick={() => {
+              setStatus("Completed");
+            }}
+          >
+            Completed
+          </span>
+        </div>
+        <div className='flex flex-col gap-2'>
+          {orders.map((order) => (
+            <div className="border-2 rounded-md">Hi</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
