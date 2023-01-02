0b05a2764f6dec3cb97469389190cc76c3561b3b
Dewaley <adewaleademilua@gmail.com>
2023-01-02 21:36:02 +0100

    new component for each order

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
+
+const Order = ({ order }) => {
+  function numberWithCommas(x) {
+    return x?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  const navigate = useNavigate();
+
+  const [showToast, setShowToast] = useState(false);
+  const isoDate = order?.updated_at;
+  const date = new Date(isoDate);
+  const options = { month: "short", day: "2-digit", year: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(order.order_transaction_id);
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+      }, 3000);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className='border-2 rounded-md p-2 md:p-4 flex flex-col relative'>
+      {showToast && <div className='toast p-1 text-xs'>Text copied</div>}
+      <div className='flex justify-between items-center border-b-2 py-4'>
+        <span className='bg-[#FFA500] p-1 text-white/70 rounded text-xs sm:text-sm mr-1'>
+          Processing
+        </span>
+        <div className='flex flex-col gap-2 text-neutral-300 text-xs sm:text-sm'>
+          <span>Order-Date: {formattedDate}</span>
+          <span className='flex gap-3 items-center'>
+            <span className='block sm:hidden'>
+              Order-ID: {order.order_transaction_id.substring(0, 5) + "..."}
+            </span>
+            <span className='hidden sm:block'>
+              Order-ID: {order.order_transaction_id.substring(0, 10) + "..."}
+            </span>
+            <span className='text-greenish text-xs' onClick={handleCopy}>
+              Copy
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className='flex justify-between items-center py-4 text-xs md:text-sm'>
+        <div className='flex items-center gap-2 mr-4'>
+          <img
+            src={
+              "https://media.tryeazzy.co/" +
+              order.order_details[0].productDetails.product_image
+            }
+            alt=''
+            className='w-[40%] sm:w-[60%]'
+          />
+          <div className='flex flex-col gap-2'>
+            <span>{order.order_details[0].productDetails.product_name}</span>
+            <span>
+              &#x20A6;
+              {numberWithCommas(
+                order.order_details[0].productDetails.product_price *
+                  order.order_details[0].quantity
+              )}
+            </span>
+            <span>{order.order_details[0].productDetails.product_unit}</span>
+          </div>
+        </div>
+        <div className='w-full max-w-[300px]'>
+          <Button
+            content={"View order"}
+            large
+            onClick={() => {
+              navigate(`/order/${order.order_transaction_id}`);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -3,25 +3,14 @@ import { useState, useEffect } from "react";
 import AuthServices from "../../services/AuthServices";
 import Button from "../../components/Button/Button";
 import { useNavigate } from "react-router-dom";
+import Order from "../../components/Order/Order";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [user, setUser] = useState({});
   const [status, setStatus] = useState("All");
-  const [showToast, setShowToast] = useState(false);
-  const navigate = useNavigate();
+  
 
-  const handleCopy = async () => {
-    try {
-      await navigator.clipboard.writeText(2);
-      setShowToast(true);
-      setTimeout(() => {
-        setShowToast(false);
-      }, 3000);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   useEffect(() => {
     AuthServices.fetchUser().then((res) => {
@@ -99,78 +88,9 @@ const Orders = () => {
           </span>
         </div>
         <div className='flex flex-col gap-2'>
-          {orders.map((order) => {
-            const isoDate = order.updated_at;
-            const date = new Date(isoDate);
-            const options = { month: "short", day: "2-digit", year: "numeric" };
-            const formattedDate = date.toLocaleDateString("en-US", options);
-            return (
-              <div className='border-2 rounded-md p-2 md:p-4 flex flex-col relative'>
-                {showToast && (
-                  <div className='toast p-1 text-xs'>
-                    Text copied
-                  </div>
-                )}
-                <div className='flex justify-between items-center border-b-2 py-4'>
-                  <span className='bg-[#FFA500] p-1 text-white/70 rounded text-xs sm:text-sm mr-1'>
-                    Processing
-                  </span>
-                  <div className='flex flex-col gap-2 text-neutral-300 text-xs sm:text-sm'>
-                    <span>Order-Date: {formattedDate}</span>
-                    <span className='flex gap-3 items-center'>
-                      <span className='block sm:hidden'>
-                        Order-ID:{" "}
-                        {order.order_transaction_id.substring(0, 5) + "..."}
-                      </span>
-                      <span className='hidden sm:block'>
-                        Order-ID:{" "}
-                        {order.order_transaction_id.substring(0, 10) + "..."}
-                      </span>
-                      <span
-                        className='text-greenish text-xs'
-                        onClick={handleCopy}
-                      >
-                        Copy
-                      </span>
-                    </span>
-                  </div>
-                </div>
-                <div className='flex justify-between items-center py-4 text-xs md:text-sm'>
-                  <div className='flex items-center gap-2 mr-4'>
-                    <img
-                      src={
-                        "https://media.tryeazzy.co/" +
-                        order.order_details[0].productDetails.product_image
-                      }
-                      alt=''
-                      className='w-[40%] sm:w-[60%]'
-                    />
-                    <div className='flex flex-col gap-2'>
-                      <span>
-                        {order.order_details[0].productDetails.product_name}
-                      </span>
-                      <span>
-                        {order.order_details[0].productDetails.product_price *
-                          order.order_details[0].quantity}
-                      </span>
-                      <span>
-                        {order.order_details[0].productDetails.product_unit}
-                      </span>
-                    </div>
-                  </div>
-                  <div className='w-full max-w-[300px]'>
-                    <Button
-                      content={"View order"}
-                      large
-                      onClick={() => {
-                        navigate(`/order/${order.order_transaction_id}`);
-                      }}
-                    />
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {orders.map((order) => (
+            <Order order={order} />
+          ))}
         </div>
       </div>
     </div>
